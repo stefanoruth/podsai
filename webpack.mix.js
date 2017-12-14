1e80646273f1d647f927f6a6c624f702e337f6a7
Stefano Ruth <stefano6262@gmail.com>
2017-12-14 17:51:18 +0100

    Add versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,3 +18,7 @@ mix.js('resources/assets/js/app.js', 'public')
         processCssUrls: false,
         postCss: [ tailwindcss('./tailwind.js') ],
   });
+
+if (mix.inProduction()) {
+    mix.version();
+}
