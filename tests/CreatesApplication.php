f42d9f33a6abe44a8b1ef22d0ffe3c6cf55add20
Stefano Ruth <stefanoruth@users.noreply.github.com>
2018-02-10 15:29:37 +0100

    Fix test scripts

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\Console\Kernel;
 
-abstract class TestCase extends BaseTestCase
+trait CreatesApplication
 {
-    use CreatesApplication;
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        Hash::driver('bcrypt')->setRounds(4);
+
+        return $app;
+    }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,21 +3,8 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Contracts\Console\Kernel;
 
 abstract class TestCase extends BaseTestCase
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(Kernel::class)->bootstrap();
-
-        return $app;
-    }
+    use CreatesApplication;
 }
