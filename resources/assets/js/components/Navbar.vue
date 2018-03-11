fb115613af51b81f69e55e4435224ae908f7f67e
Stefano Ruth <stefanoruth@users.noreply.github.com>
2018-03-11 16:30:08 +0100

    Close menu bar when hitting profile page

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -2,7 +2,7 @@
     <div class="bg-white border-t-8 border-orange">
         <nav class="max-w-2xl mx-auto relative select-none md:flex md:items-stretch">
             <div class="flex flex-no-shrink items-stretch">
-                <router-link to="/" class="flex-no-grow flex-no-shrink relative p-4 md:p-8 leading-normal text-black text-2xl font-black text-grey-darkest no-underline flex items-center" @click.native="mobileMenu = false">Podsai</router-link>
+                <router-link to="/" @click.native="mobileMenu = false" class="flex-no-grow flex-no-shrink relative p-4 md:p-8 leading-normal text-black text-2xl font-black text-grey-darkest no-underline flex items-center">Podsai</router-link>
                 <button class="block md:hidden cursor-pointer ml-auto relative p-4" @click="mobileMenu = !mobileMenu">
                     <div class="h-6 w-6">
                         <svg v-show="!mobileMenu" class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
@@ -20,8 +20,8 @@
                             <svg class="fill-current h-4 w-4 pt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>    
                         </div>
                         <div class="md:absolute pin-r pin-t-100 md:hidden min-w-full group-hover:block bg-white md:shadow z-20 md:rounded-b md:py-1">
-                            <router-link :to="{name: 'profile'}" class="menu-item px-4">Profile</router-link>
-                            <a :href="logout" class="menu-item px-4">Logout</a>
+                            <router-link :to="{name: 'profile'}" @click.native="mobileMenu = false" class="menu-item px-4">Profile</router-link>
+                            <a :href="logout" @click.native="mobileMenu = false" class="menu-item px-4">Logout</a>
                         </div>
                     </div>
                     
