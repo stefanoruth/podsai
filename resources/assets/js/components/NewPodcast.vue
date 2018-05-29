84ce9fe947a0a5b408d67b2980e6fb855e8016e2
Stefano Ruth <stefanoruth@users.noreply.github.com>
2018-05-29 17:09:21 +0200

    New search result look

--- resources/assets/js/components/NewPodcast.vue
+++ resources/assets/js/components/NewPodcast.vue
@@ -1,12 +1,15 @@
 <template>
-    <modal @close="$emit('close')" :cssScope="'w-full md:max-w-2/5'" :loading="loading">
+    <modal @close="$emit('close')" :cssScope="'w-full md:max-w-3/5'" :loading="loading">
         <div class="border-b">
             <input class="text-2xl text-black p-4 w-full" v-model="searchQuery" type="text" @keyup.enter="searchItunes" autofocus placeholder="Search for podcasts..." autocomplete="off">
         </div>
         
         <div v-show="searchResults.length > 0" class="flex flex-col mt-4 h-full overflow-y-scroll">
-            <div v-for="(result, i) in searchResults" :key="i" class="px-4 mb-2 py-2 hover:bg-grey-lighter text-black flex justify-between overflow-hidden">
-                <div class="flex-1" :title="result.feedUrl">
+            <div v-for="(result, i) in searchResults" :key="i" class="px-4 mb-2 py-2 hover:bg-grey-lighter text-black flex justify-between overflow-hidden max-w-100">
+                <div class="flex items-center pr-2">
+                    <img class="block w-10 h-10" :src="result.artworkUrl30" :alt="result.collectionName">
+                </div>
+                <div class="flex-1 pr-2" :title="result.feedUrl">
                     <div class="mb-1">{{ result.collectionName }}</div>
                     <div class="text-sm text-grey-dark">{{  result.artistName }}</div>
                 </div>
@@ -34,7 +37,7 @@
                 axios.get('https://itunes.apple.com/search', {
                     params: {
                         media: 'podcast',
-                        limit: 10,
+                        limit: 5,
                         term: this.searchQuery,
                     },
                     headers: {
